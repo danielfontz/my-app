f329b37e7baf50809c68d8207b4340c65acd2a3d
DANIEL DAVID FUENTES CONDORI <daniel.dfcondori@cas0681363ma-1.local>
2026-09-01 09:44:55 -0300

    add modal

---
+++ src/components/modal/index.jsx
@@ -0,0 +1,20 @@
+// namespace import
+import * as DM from "./style"
+import { useNavigation } from "@react-navigation/native"
+import { FontAwesome6 } from "@expo/vector-icons"
+
+const DetailsModal = () => {
+    const navigation = useNavigation()
+    return (
+        <DM.Overlay onPress={() => navigation.goBack()}>
+            <DM.Container>
+                <DM.Title>Title</DM.Title>
+                <DM.Description>DMescription</DM.Description>
+                <DM.CloseBtn onPress={() => navigation.goBack()}>
+                    <DM.CloseBtnText>Close</DM.CloseBtnText>
+                </DM.CloseBtn>
+            </DM.Container>
+        </DM.Overlay>
+    )
+}
+export default DetailsModal
